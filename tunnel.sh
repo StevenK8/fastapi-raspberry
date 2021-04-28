a85c5529f525818d438f4e8fc16593404f2c6937
StevenK8 <43873889+StevenK8@users.noreply.github.com>
2021-04-28 22:47:53 +0200

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -6,16 +6,4 @@ do
     echo "Fermeture du tunnel $(date)"
     sleep 1
 done
-sudo chmod +x tunnel.sh
- 
-systemctl daemon-reload
- 
-systemctl enable sshtunnel.service
-sudo ssh-keygen -t ecdsa -b 521 -f /root/.ssh/id_ecdsa_tunnel
- 
-sudo chmod 600 /root/.ssh/id_ecdsa_tunnel
-sudo chmod 600 /root/.ssh/id_ecdsa_tunnel.pub
- 
-sudo ssh-copy-id -i /root/.ssh/id_ecdsa_tunnel.pub tunnel@ryzen.ddns.net -p 6622
- 
-sudo ssh -i /root/.ssh/id_ecdsa_tunnel -R \*8000:localhost:8000 -N tunnel@ryzen.ddns.net -p 6622
+
